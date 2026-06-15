a0c98c5d7a28742fc1a340f83f1702d30b0c8a1f
Santiago Hernández Ávila <shdeza12@gmail.com>
2026-06-14 19:58:04 -0500

    Fix: Update deprecated tf2_geometry_msgs header for ROS 2 Humble
    - Changed from tf2_geometry_msgs/tf2_geometry_msgs.h (deprecated)
    - To tf2_geometry_msgs/tf2_geometry_msgs.hpp (current)
    - Resolves build warning in ROS 2 Humble

--- Robot/aws-deepracer/deepracer_gazebo/src/gazebo_ros_deepracer_drive.cpp
+++ Robot/aws-deepracer/deepracer_gazebo/src/gazebo_ros_deepracer_drive.cpp
@@ -30,7 +30,7 @@
 #include <std_msgs/msg/float64_multi_array.hpp>
 #include <sdf/sdf.hh>
 
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 
